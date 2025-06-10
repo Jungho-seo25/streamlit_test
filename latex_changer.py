08ce5525553b13ddc7e12ed0ef6206a16b226807
Jungho-seo25 <sjungho423@gmail.com>
2025-06-11 08:54:43 +0900

    Add files via upload

--- latex_changer.py
+++ latex_changer.py
@@ -4,6 +4,7 @@ st.title("Latex 시각화 페이지")
 
 user_input = ""
 user_input = st.text_input("latex문을 입력하세요")
+cleaned = ''.join(user_input.split())
 
 if user_input:
-    st.latex(f'{user_input}')
+    st.latex(f'{cleaned}')
